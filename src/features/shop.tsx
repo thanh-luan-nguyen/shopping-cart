66eb74af9d62ebefcdaf601f5bfa8f84c4c72c10
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-08-27 18:18:16 +0900

    .

--- src/features/shop.tsx
+++ src/features/shop.tsx
@@ -118,14 +118,6 @@ const shopSlice = createSlice({
   name: 'shop',
   initialState: { value: initialStateValue },
   reducers: {
-    SET_PRODUCTS: (state, action) => {
-      const products = action.payload.filter(
-        (product: any) =>
-          product.category === `men's clothing` ||
-          product.category === `women's clothing`
-      )
-      state.value = { ...state.value, products: products }
-    },
     ADD_TO_CART: (state, action) => {
       const id = action.payload
       if (state.value.cart.some((p: any) => p.id === id)) {
@@ -164,4 +156,4 @@ const shopSlice = createSlice({
 
 export default shopSlice.reducer
 
-export const { SET_PRODUCTS, ADD_TO_CART, REMOVE_FROM_CART } = shopSlice.actions
+export const { ADD_TO_CART, REMOVE_FROM_CART } = shopSlice.actions
